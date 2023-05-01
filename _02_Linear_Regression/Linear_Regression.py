928260e39ea9e77afb87a570881f293121bf5f91
Xubanghao <51075689+shuoyue21@users.noreply.github.com>
2023-05-01 19:06:59 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -10,11 +10,26 @@ except ImportError as e:
 
 def ridge(data):
     pass
-    
-def lasso(data):
-    pass
+
+def __init__(self, alpha=1e-12, max_iter=1000000, tol=0.0001):
+        self.alpha = alpha  # 正则化系数
+        self.max_iter = max_iter  # 最大迭代次数
+        self.tol = tol  # 收敛阈值
+def lasso(self,data):
+    X,y = read_data();
+    m,n = X.shape;
+    self.theta = np.zeros(n);
+    for i in range(self.max_iter):
+        #Calculate the gradient
+        grad = np.dot(X.T, np.dot(X, self.theta) - y) + self.alpha * np.sign(self.theta)
+        #Update theta
+        self.theta -= self.alpha * grad
+        #Stopping condition
+        if np.linalg.norm(grad) < self.tol:
+            break
+    return np.dot(X,self.theta)
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
-    return x, y
+    return x, 
