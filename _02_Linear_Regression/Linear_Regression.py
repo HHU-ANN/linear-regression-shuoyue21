02900164b7d76fb8c8551281a057e281e912c527
Xubanghao <51075689+shuoyue21@users.noreply.github.com>
2023-05-01 20:33:15 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -15,7 +15,7 @@ def ridge(data):
     return w @ data
 def lasso(data):
     alpha = 1e-12  # 正则化系数
-    max_iter = 5000000  # 最大迭代次数
+    max_iter = 7000000  # 最大迭代次数
     tol = 0.000001  # 收敛阈值
     X,y = read_data()
     m,n = X.shape
